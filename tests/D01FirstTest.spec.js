c1d4683a44fdaf30a4f5a936478a6deb7b0e4423
Ankush <ankushvankore@gmail.com>
2026-08-14 09:10:21 +0530

    Date: 14 Aug 2026

--- tests/D01FirstTest.spec.js
+++ tests/D01FirstTest.spec.js
@@ -1,4 +1,22 @@
 //Import playwritht library
+
+/*
+Execution ways
+1. npx playwright test => Will execute all the tests from the tests folder
+2. npx playwright test tests/Name of test.spec.js => single test in headless mode
+3. npx playwright test tests/Name of test.spec.js --headed => Single test in headed mode
+4. npx playwright test tests/Name of test.spec.js --headed --project=chromium => Execute tc on chrome
+
+5. for execution only on msedge
+write the code inside the test()
+let browser = await chromium.launch({headed:false, channel: "msedge"})
+this time just call
+npx playwright test tests/Name of test.spec.js 
+on the terminal
+
+*/
+
+
 import {test} from "@playwright/test";
 
 test("Test case1", ()=>{
--- tests/D04TestOnEdge.spec.js
+++ tests/D04TestOnEdge.spec.js
@@ -8,3 +8,51 @@ test("Test on Edge", async ({})=>{
     let p1 = await c1.newPage();
     await p1.goto("https://www.google.com");
 })
+
+/*
+for execution only on msedge
+write the code inside the test()
+let browser = await chromium.launch({headed:false, channel: "msedge"})
+this time just call
+npx playwright test tests/Name of test.spec.js 
+on the terminal
+
+but before this comment the following code from playwrithg.config.js
+
+ Configure projects for major browsers 
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
+
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
+
+    /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
+
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // },
+ 
